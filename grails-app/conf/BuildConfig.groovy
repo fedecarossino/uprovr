3094c03ca8cfcb8357619f06d0d8a9748ef7bac8
Fede Carossino <federico.carossino@mercadolibre.com>
2015-01-27 12:43:57 -0300

    nueva version sqldriver

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,7 +49,7 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-         runtime 'mysql:mysql-connector-java:5.1.27'
+         runtime 'mysql:mysql-connector-java:5.1.33'
         // runtime 'org.postgresql:postgresql:9.3-1100-jdbc41'
 		compile 'org.twitter4j:twitter4j-core:4.0.2'
 		compile('org.codehaus.groovy.modules.http-builder:http-builder:0.5.0') {
